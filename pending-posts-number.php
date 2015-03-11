f2e2577dd5db7e910a1f39c0dba92378edec062f
NikV <techvoltz@gmail.com>
2015-03-11 06:32:14 -0500

    Plugin Info

--- pending-posts-number.php
+++ pending-posts-number.php
@@ -1,12 +1,20 @@
 <?php
 /**
- * Plugin Name: Admin Menu Pending Posts
+ * Plugin Name:     Admin Menu Pending Post
+ * Plugin URI:      @todo
+ * Description:     Adds a bubble in the admin menu with the number of pending posts.
+ * Version:         1.0.0
+ * Author:          Nikhil Vimal
+ * Author URI:      http://nik.techvoltz.com
+ *
+ * @copyright       Copyright Nikhil Vimal (c) 2014
  */
 
 /**
  * Main Function adding the bubble with the number to the admin menu
  */
 function add_pending_admin_bubble() {
+
 	global $menu;
 
 	$custom_post_count = wp_count_posts('post');
